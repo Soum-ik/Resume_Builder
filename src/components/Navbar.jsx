ec8c8df26f48da629e9e55836b3343fbe694a7e6
soumik <sarkarsoumik215@gmail.com>
2023-10-19 12:29:56 +0600

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,27 @@
-import Logo from "../image/logo.png";
 import { useState } from "react";
-import { AiOutlineAlignRight } from "react-icons/ai";
-import { GrClose } from "react-icons/gr";
+import Logo from "../asset/logo.png";
 
 const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
+  function handleChange() {
+    setIsOpen(!isOpen);
+  }
   return (
     <div className={` flex items-center justify-between w-full drop-shadow-lg`}>
-      <img src={Logo} className=" w-[180px] -my-14" alt="" />
+      <img src={Logo} className=" w-[180px] -my-14 " alt="" />
       <ul className=" hidden sm:flex justify-between items-center  gap-4 cursor-pointer select-none">
         <li className=" hover:bg-slate-900/5 px-4 py-3 rounded-md text-[17px] text-secondary font-medium">
-          Log In
+          <a href="">Log In</a>
         </li>
         <li className="botton px-4 py-3 rounded-lg font-semibold text-[17px] ">
-          Sing Up
+          <a href="">Sing Up</a>
         </li>
       </ul>
       {/* moblie part */}
-      <div>
-
+      <div className=" sm:hidden flex  justify-end items-center">
+        <li className="botton p-2 xxxS:px-4 xxxS:py-3 rounded-lg font-semibold text-[17px] list-none">
+          <a href="">Sing Up</a>
+        </li>
       </div>
     </div>
   );
