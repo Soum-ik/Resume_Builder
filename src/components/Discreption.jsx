1cfb6cb4114a48da7a7763c0ee5430c1137531fb
soumik <sarkarsoumik215@gmail.com>
2023-10-20 03:17:10 +0600

    hero done

--- src/components/Discreption.jsx
+++ src/components/Discreption.jsx
@@ -1,14 +1,24 @@
-import styles from "../style/Style";
+import styles, { layout } from "../style/Style";
 import Resume from "../asset/resume.svg";
+import {Motivations} from "../Contants/contant";
+import { useState } from "react";
+import Button from "../components/Button"
 
 const Discreption = () => {
+  const [short, setShort] = useState(false)
   return (
-    <div className={`${styles.paddingX} ${styles.boxWidth}`}>
-      <div>
+    <div className={` ${styles.boxWidth}`}>
+      <div className={`${layout.section} gap-10`}>
         <img src={Resume} alt="" />
+        <div className="pt-10">
+          <h5 className=" text-primary font-semibold">RESUME MAKER</h5>
+          <h2 className="text-[40px] font-bold leading-[3rem] max-w-[500px]">Every Great Career Begins with a <span className="text-primary">Solid Resume</span></h2>
+          <p className="py-10 text-dimWhite">{Motivations}</p>
+          <Button text={"Create a Resume"}/>
+        </div>
       </div>
-      <div>
-        <h5 className=" text-primary font-medium">RESUME MAKER</h5>
+      <div className={`${styles.marginX} ${styles.flexCenter} bg-primary/5`}>
+        Build and Develop by Soumik
       </div>
     </div>
   );
