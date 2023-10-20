f3b2de0e654484c9643ec4ea638c1029958a569c
soumik <sarkarsoumik215@gmail.com>
2023-10-20 12:12:26 +0600

    auth start

--- src/auth/authSetUp.jsx
+++ src/auth/authSetUp.jsx
@@ -7,15 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCmyD6LkC4I3NaPY6E7gJOsX4h0zmigtyc",
-  authDomain: "developer-9o9.firebaseapp.com",
-  databaseURL:
-    "https://developer-9o9-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "developer-9o9",
-  storageBucket: "developer-9o9.appspot.com",
-  messagingSenderId: "563748231896",
-  appId: "1:563748231896:web:12dccaa8e6676997e0d5ad",
-  measurementId: "G-55TQR1KZ43",
+  apiKey: "AIzaSyCSENNEOO2MOk7VxVr2KM22NFSSkYjBulc",
+  authDomain: "resume-21974.firebaseapp.com",
+  projectId: "resume-21974",
+  storageBucket: "resume-21974.appspot.com",
+  messagingSenderId: "293671102421",
+  appId: "1:293671102421:web:2705d235270eb378493f5c",
+  measurementId: "G-WFF27D4GGL"
 };
 
 // Initialize Firebase
