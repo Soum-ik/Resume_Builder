04540118ab982e4ddb5419da7e7fa7b5ac41586a
soumik <sarkarsoumik215@gmail.com>
2023-11-12 14:41:55 +0600

    fix skeleton on image

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Skeleton from '@mui/material/Skeleton';
 const Button = ({ text, width, page, style }) => (
   <button
     className={`botton p-1 sm:p-2 xxxS:px-4 xxxS:py-3 sm:rounded-lg rounded-md font-semibold text-[17px] list-none ${width} ${
--- src/components/TemplateImage.jsx
+++ src/components/TemplateImage.jsx
@@ -1,25 +1,41 @@
+import React, { useEffect, useState } from "react";
+import Skeleton from "@mui/material/Skeleton";
 import Template_Img from "../asset/resume.jpg";
 import Template_Img2 from "../asset/resume6.webp";
 import Template_Img3 from "../asset/resume5.jpg";
 import Button from "./Button";
 
 const Template = ({ img }) => {
-  return (
-    <div className=" relative  group">
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []); // Add an empty dependency array to ensure the effect runs only once
+
+  return loading ? (
+    <Skeleton height={600} placeholder="loading"/>
+  ) : (
+    <div className="relative group">
       {img === "Template_Img" ? (
         <img
-          className=" aspect-square relative h-fit object-cover  group-hover:blur-[2px] rounded-md"
+          className="aspect-square relative h-fit object-cover group-hover:blur-[2px] rounded-md"
           src={Template_Img}
+          alt="Template Image"
         />
       ) : img === "Template_Img2" ? (
         <img
           src={Template_Img2}
           className="aspect-square relative object-cover h-fit group-hover:blur-[2px] rounded-md"
+          alt="Template Image 2"
         />
       ) : (
         <img
           src={Template_Img3}
-          className="aspect-square relative object-cover  h-fit group-hover:blur-[2px] rounded-md"
+          className="aspect-square relative object-cover h-fit group-hover:blur-[2px] rounded-md"
+          alt="Template Image 3"
         />
       )}
       <Button
@@ -30,4 +46,5 @@ const Template = ({ img }) => {
     </div>
   );
 };
+
 export default Template;
