ca3afc95098a4140f65b7c7ae9a142890ec8931f
soumik <sarkarsoumik215@gmail.com>
2023-11-16 00:59:57 +0600

    fix/singUp & logIn page bug sovling

--- src/Sing&Login/Login.jsx
+++ src/Sing&Login/Login.jsx
@@ -1,23 +1,83 @@
-import System from "../components/System";
 import styles from "../style/Style";
+import { AiFillEye } from "react-icons/ai";
+
 import { Link } from "react-router-dom";
 
 const logIn = () => {
   return (
-    <div className={`${styles.marginX} `}>
-      <div className={`${styles.paddingY} ${styles.flexCenter}`}>
-        <div className=" text-center ">
-          <h1 className=" text-primary font-bold text-[40px]">Log In</h1>
-          <p className=" text-dimWhite text-[20px]">
-            We are happy to see you back!
-          </p>
-          <System icon={"Google"} name={"Google"} gap={"10"} />
-          <System icon={"Facebook"} name={"Facebook"} gap={"10"} />
+    <div className={`${styles.marginX}`}>
+      <div className={`${styles.paddingY} ${styles.flexCenter} `}>
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img
+              className="mx-auto h-10 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt="Your Company"
+            />
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Log in to your account
+            </h2>
+          </div>
 
-          <Link to="/singUpPage" className=" text-secondary ">
-            I am not registered —{" "}
-            <span className=" text-primary font-medium">Sign Up</span>
-          </Link>
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className=" outline-none block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 hover:ring-2 hover:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Password
+                  </label>
+                </div>
+                <div className=" hover:ring-2 hover:ring-indigo-600 border ring-1 ring-gray-300 mt-2 flex rounded-md flex-1 items-center justify-centerring-inset placeholder:text-gray-400 ">
+                  <input
+                    id="password"
+                    name="password"
+                    type="text"
+                    autoComplete="current-password"
+                    required
+                    className="   outline-none block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm  ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                  />
+                  <AiFillEye size={30} className="p-2" />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Log in
+                </button>
+              </div>
+            </form>
+            <p className=" text-center pt-5">
+              <Link to="/singUppage" className="text-secondary my-4">
+                I am not registered —{" "}
+                <span className=" text-primary font-medium">Sing In </span>
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- src/Sing&Login/SingUp.jsx
+++ src/Sing&Login/SingUp.jsx
@@ -1,93 +1,21 @@
+import System from "../components/System";
 import styles from "../style/Style";
-import { AiFillEye } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const SingUp = () => {
   return (
-    <div className={`${styles.marginX}`}>
-      <div className={`${styles.paddingY} ${styles.flexCenter} `}>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              className="mx-auto h-10 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
-            />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign in to your account
-            </h2>
-          </div>
+    <div className={`${styles.marginX} `}>
+      <div className={`${styles.paddingY} ${styles.flexCenter}`}>
+        <div className=" text-center ">
+          <h1 className=" text-primary font-bold text-[40px]">Sing In</h1>
+          <p className=" text-dimWhite text-[20px]">Welcome to Hello Builder</p>
+          <System icon={"Google"} name={"Google"} gap={"10"} />
+          <System icon={"Facebook"} name={"Facebook"} gap={"10"} />
 
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" action="#" method="POST">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                    className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-              <div>
-                <div className="flex items-center justify-between">
-                  <label
-                    htmlFor="password"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Password
-                  </label>
-                  <div className="text-sm">
-                    <a
-                      href="#"
-                      className="font-semibold text-indigo-600 hover:text-indigo-500"
-                    >
-                      Forgot password?
-                    </a>
-                  </div>
-                </div>
-                <div className="mt-2 flex rounded-md  flex-1 items-center justify-centerring-inset ring-1 ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    className=" focus:none w-full  px-2 py-1.5 text-gray-900 shadow-sm  sm:text-sm sm:leading-6"
-                  />
-                  <AiFillEye size={30} className="p-2" />
-                </div>
-              </div>
-
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign in
-                </button>
-              </div>
-            </form>
-
-            <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{" "}
-              <a
-                href="#"
-                className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-              >
-                Start a 14 day free trial
-              </a>
-            </p>
-          </div>
+          <Link to="/logInPage" className=" text-secondary ">
+            I am registered —{" "}
+            <span className=" text-primary font-medium">log In</span>
+          </Link>
         </div>
       </div>
     </div>
