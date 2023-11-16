937b89ee488437197ebcdf2401c385adbec84192
soumik <sarkarsoumik215@gmail.com>
2023-11-17 03:20:19 +0600

    fix/vercel route

--- src/components/System.jsx
+++ src/components/System.jsx
@@ -23,7 +23,7 @@ const System = ({ name, icon }) => {
       type="submit"
       className={` last:pb-10 cursor-pointer min-w-[250px] ss:min-w-[300px] sm:min-w-[350px] md:min-w-[400px] mt-5 flex flex-1 flex-row items-center justify-between bg-slate-400/25 hover:bg-slate-400/40 duration-500 px-5 py-3 rounded-xl`}
     >
-      <div className=" gap-2 flex flex-1 items-center justify-start" onClick={}>
+      <div className=" gap-2 flex flex-1 items-center justify-start">
         {icons}
         <h2 className=" text-secondary font-medium ">{name}</h2>
       </div>
